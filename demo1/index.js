3bf8c7af4167bfceec1b052ba8cdd08a7ed61a98
Yin Shen <shenyinabcd@msn.cn>
2017-09-04 22:00:49 +0800

    update code

---
+++ demo1/index.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#foo').on('click', function() {
+        var content2 = '<img width="800" height="500" src="img/cat_1.jpg">';
+        TINY.box.show(content2, 0, 0, 0, 1);
+    });
+});
